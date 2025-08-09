97844daf2f293c448322063e171dab6d8c4dbf87
JacksonK <jackmkirchner@gmail.com>
2025-08-09 13:27:42 -0500

    Initial Commit

---
+++ src/Game.cpp
@@ -0,0 +1,17 @@
+#include "../external/SFML/include/SFML/Window/Window.hpp"
+//
+// Created by jkirc on 8/7/2025.
+//
+class Game {
+public:
+    Game() {
+
+    }
+
+    void run() {
+
+    }
+
+private:
+    sf::Window
+};
