c888d5afada7cb74567328f4edef4c7901fd5b5a
JacksonK <jackmkirchner@gmail.com>
2025-08-17 20:58:51 -0500

    Added Plays and formations. Haven't implemented them yet, will do tomorrow.

---
+++ src/scene/football/team/playcalling/Formation.h
@@ -0,0 +1,8 @@
+//
+// Created by jkirc on 8/17/2025.
+//
+
+#ifndef FORMATION_H
+#define FORMATION_H
+
+#endif //FORMATION_H
---
+++ src/scene/football/team/playcalling/Play.h
@@ -0,0 +1,8 @@
+//
+// Created by jkirc on 8/17/2025.
+//
+
+#ifndef PLAY_H
+#define PLAY_H
+
+#endif //PLAY_H
